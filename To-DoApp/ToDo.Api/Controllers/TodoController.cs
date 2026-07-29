b2cc2398d151757ab938a178f93fc65a68c6b63c
Elvin-Aliyev201 <elvinaliyevv4483@gmail.com>
2026-07-30 00:39:06 +0400

    backend is done

---
+++ To-DoApp/ToDo.Api/Controllers/TodoController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Api.Dtos;
+using ToDo.Api.Services;
+
+namespace ToDo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoDto>> Create(
+             CreateTodoDto dto,
+             CancellationToken cancellationToken)
+        {
+            var createdTodo = await _todoService.CreateAsync(dto, cancellationToken);
+
+            return CreatedAtAction(nameof(GetById), new { id = createdTodo.Id }, createdTodo);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TodoDto>>> GetAll(
+            CancellationToken cancellationToken)
+        {
+            var todos = await _todoService.GetAllAsync(cancellationToken);
+
+            return Ok(todos);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TodoDto>> GetById(
+            int id,
+            CancellationToken cancellationToken)
+        {
+            var todo = await _todoService.GetByIdAsync(id, cancellationToken);
+
+            if (todo is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(
+            int id,
+            UpdateTodoDto dto,
+            CancellationToken cancellationToken)
+        {
+            var updated = await _todoService.UpdateAsync(id, dto, cancellationToken);
+
+            if (!updated)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(
+            int id,
+            CancellationToken cancellationToken)
+        {
+            var deleted = await _todoService.DeleteAsync(id, cancellationToken);
+
+            if (!deleted)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
--- To-DoApp/ToDo.Api/Program.cs
+++ To-DoApp/ToDo.Api/Program.cs
@@ -35,12 +35,16 @@ namespace ToDo.Api
        
             builder.Services.AddOpenApi();
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
-
 
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+     
+                app.UseSwagger();
+                app.UseSwaggerUI(); 
+         
             }
 
             app.UseHttpsRedirection();
