1a437e9d0332f2922eedee8002494321da90bfd1
Elvin-Aliyev201 <elvinaliyevv4483@gmail.com>
2026-07-30 21:28:30 +0400

    Fixing programcs

--- backend/To-DoApp/ToDo.Api/Program.cs
+++ backend/To-DoApp/ToDo.Api/Program.cs
@@ -63,13 +63,12 @@ namespace ToDo.Api
             builder.Services.AddSwaggerGen();
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
+          
      
                 app.UseSwagger();
                 app.UseSwaggerUI(); 
          
-            }
+            
 
             app.UseHttpsRedirection();
 
