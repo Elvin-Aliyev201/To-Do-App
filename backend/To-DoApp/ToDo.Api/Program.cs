5b51671d5da07b9745271f459f2fe6749d4af959
Elvin-Aliyev201 <elvinaliyevv4483@gmail.com>
2026-07-30 20:21:52 +0400

    Finish

--- backend/To-DoApp/ToDo.Api/Program.cs
+++ backend/To-DoApp/ToDo.Api/Program.cs
@@ -1,4 +1,5 @@
 
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using ToDo.Api.Data;
 using ToDo.Api.Services;
@@ -9,12 +10,24 @@ namespace ToDo.Api
     {
         public static void Main(string[] args)
         {
+            Env.Load();
             var builder = WebApplication.CreateBuilder(args);
             var databaseHost = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
             var databasePort = Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5432";
             var databaseName = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "TodoDb";
-            var databaseUser = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "postgres";
-            var databasePassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "postgres123";
+
+            var databaseUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+            var databasePassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+            if (string.IsNullOrWhiteSpace(databaseUser))
+            {
+                throw new InvalidOperationException("POSTGRES_USER environment variable is missing.");
+            }
+
+            if (string.IsNullOrWhiteSpace(databasePassword))
+            {
+                throw new InvalidOperationException("POSTGRES_PASSWORD environment variable is missing.");
+            }
 
             var connectionString =
                 $"Host={databaseHost};" +
