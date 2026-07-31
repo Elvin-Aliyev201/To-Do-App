e291b688e8e0e24c6add51f25a5aee15d8cc444d
Elvin-Aliyev201 <elvinaliyevv4483@gmail.com>
2026-07-31 10:32:48 +0400

    AUto migration

--- backend/To-DoApp/ToDo.Api/Program.cs
+++ backend/To-DoApp/ToDo.Api/Program.cs
@@ -88,6 +88,12 @@ namespace ToDo.Api
           
             app.MapControllers();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
             app.Run();
         }
     }
